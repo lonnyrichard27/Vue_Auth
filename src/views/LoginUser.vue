a3dd0eb2661f484b387b9573a9864868f599f04f
Oluwaloni Richard <lonnyrichard77@gmail.com>
2020-08-10 00:20:21 +0100

    commit

---
+++ src/views/LoginUser.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <form @submit.prevent="login">
+        <label for="email">
+            Email:    
+        </label> 
+        <input type="text" v-model="email" />
+
+        <label for="password">
+            Password:    
+        </label> 
+        <input type="password" v-model="password" />
+
+        <button type="submit" name="button">Login</button>
+        <p>{{ error }}</p>
+
+    <router-link to="/register">Dont have an account? Register</router-link>
+
+    </form>    
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            email: '',
+            password: '',
+            error: null
+        }
+    },
+    methods: {
+        login() {
+            this.$store.dispatch('login', {
+                email: this.email,
+                password: this.password
+            })
+            .then(() => {
+                this.$router.push({ name: 'dashboard'})
+            })
+            .catch((err) => {
+                this.error = err.response.data
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/RegisterUser.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+      <form @submit.prevent="register">
+        <label for="name">
+            Name:    
+        </label> 
+        <input type="text" v-model="name" />
+        <label for="name">
+            Email:    
+        </label> 
+        <input type="email" v-model="email" />
+        <label for="password">
+            Password:    
+        </label> 
+        <input type="password" v-model="password" />
+
+        <button type="submit" name="butoon">Register</button>
+    </form>    
+        <router-link to="/login">Have an account? Login</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: '',
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        register() {
+            this.$store.dispatch('register', {
+                name: this.name,
+                email: this.email,
+                password: this.password
+            })
+            .then( () => {
+                this.$router.push({ name: 'dashboard'})
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
